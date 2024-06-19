f80b7c8ceb166b830d9b2905bd91338be5d4294e
YKTECHOKILLL <111409018+YKTECHNOLOGY@users.noreply.github.com>
2024-06-19 10:43:14 +0700

    Add files via upload

---
+++ introduction/NIM2213010348.java
@@ -0,0 +1,6 @@
+
+public class NIM2213010348 {
+  public static void main(String[] args) {
+      System.out.println("MuhammadRafiMaliki");
+  }
+}
---
+++ introduction/NIM2213010371.java
@@ -0,0 +1,6 @@
+
+public class NIM2213010371 {
+  public static void main(String[] args) {
+      System.out.println("ArdyaZahiraArdhana");
+  }
+}
